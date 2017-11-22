81b5454c5e9325c84c99d0f2574824e20dafcdad
johnytiago <joaosao10@gmail.com>
2017-11-22 18:11:49 +0000

    Move member

--- natal.js
+++ natal.js
@@ -3,20 +3,18 @@ const markdownpdf = require('markdown-pdf')
 const replace  = require('stream-replace')
 const dir = "./results"
 
-const family = [
+const families = [
   ['Tia zelia', 'Zezito','Tio Joao' ,'Joao Tiago Filho','Bia'],
   ['Kiko','Andre','Tia Fatima','Tio Fernando'],
   ['Mariana','Tiago','Tio Horacio','Tia Luisa'],
-  ['Luis','Rafa','Tia Isa','Tio Luis'],
-  ['Sara']
+  ['Luis Filho','Rafa','Sara','Tia Isa','Tio Luis']
 ];
 var taken = [
 [null, null, null, null, null],
 [null, null, null, null],
 [null, null, null, null],
-[null, null, null, null],
-[null]];
-var count = [0, 0, 0, 0, 0]
+[null, null, null, null, null]];
+var count = [0, 0, 0, 0]
 
 function output(from, to) {
   fs.createReadStream("./template.md")
@@ -33,21 +31,21 @@ fs.mkdir( dir, (err) => {
   if(err)
     console.log("Folder already created")
 
-  family.forEach((casa, index) => {
+  families.forEach((casa, index) => {
     casa.forEach(membro => {
       do {
-        casaChoice = (Math.floor(Math.random() * 5))
+        casaChoice = (Math.floor(Math.random() * families.length))
       }
-      while (casaChoice === index || count[casaChoice] === family[casaChoice].length);
+      while (casaChoice === index || count[casaChoice] === families[casaChoice].length);
 
       do {
-        memberChoice = Math.floor(Math.random() * family[casaChoice].length) 
+        memberChoice = Math.floor(Math.random() * families[casaChoice].length) 
       }
       while (taken[casaChoice][memberChoice]);
 
-      taken[casaChoice][memberChoice] = true
-      count[casaChoice]++
-        output(membro, family[casaChoice][memberChoice])
+      taken[casaChoice][memberChoice] = true;
+      count[casaChoice]++;
+      output(membro, families[casaChoice][memberChoice]);
     })
   })
 })
